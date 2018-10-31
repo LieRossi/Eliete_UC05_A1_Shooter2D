6866ef5b8fa29e18f3465378c3f9e8f14c727798
LieRossi <40723203+LieRossi@users.noreply.github.com>
2018-10-31 00:00:33 -0300

    Camera
    Controle de camera

---
+++ Eliete_UC05_A1_Shooter2D/Assets/script/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private GameObject player;
+
+  
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void LateUpdate()
+    {
+        if (player != null)
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.x = player.transform.position.x;
+
+            transform.position = newPosition;
+        }
+    }
+
+}
+
